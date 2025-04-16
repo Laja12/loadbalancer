75d1d92cd9356da5daef6920ebe89bee47771c37
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-16 11:01:58 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "loadbalance1-project-bucket"  # Replace with your unique S3 bucket name
-    key    = "path/to/my/statefile/terraform.tfstate"  # Path inside the bucket to store the state
-    region = "ap-southeast-2"  # Replace with your desired AWS region
+    bucket = "openproject-bucket1"  # Replace with your unique S3 bucket name
+    key    = "path/to/my-bucket3/statefile/terraform.tfstate"  # Path inside the bucket to store the state
+    region = "us-east-1"  # Replace with your desired AWS region
     encrypt = true  # Enable encryption of the state file
    
   }
